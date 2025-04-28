17590b6348a35e5f8a2028bdca017e479a2c3702
Karma Riuk <riukkarma@gmail.com>
2025-04-28 10:20:24 +0200

    fixed bug

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 import express, { json } from 'express';
 import cors from 'cors';
-require('dotenv').config();
+import dotenv from 'dotenv';
+import routes from './routes/index.js';
 
-import routes from './routes';
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
