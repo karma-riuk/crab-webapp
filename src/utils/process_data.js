374362537fcad1cfdbc823cde27aa403bdba432a
Karma Riuk <riukkarma@gmail.com>
2025-05-11 09:35:36 +0200

    small fix on evaluate refinement

--- src/utils/process_data.js
+++ src/utils/process_data.js
@@ -48,12 +48,12 @@ export const evaluate_comments = (answers, percent_cb) => {
     return results;
 };
 
-export const evaluate_refinement = async (answers, percent_cb, finished_cb) => {
+export const evaluate_refinement = (answers, percent_cb) => {
     const total = Object.keys(answers).length;
     let i = 0;
     for (const [key, value] of Object.entries(answers)) {
         console.log(`Processing ${key}: ${value}...`);
-        await new Promise((res) => setTimeout(res, 1000));
+        // await new Promise((res) => setTimeout(res, 1000));
         console.log("Done");
         percent_cb(Math.floor((++i / total) * 100));
     }
